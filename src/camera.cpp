a47b820a7e23d8a8f80dc79da54f78158ea75d97
MONTILLET Xavier <xavierm02.net@gmail.com>
2013-11-20 20:56:34 +0100

    Added update_image, _image _distance and _up to camera.

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,7 +1,21 @@
 #include "camera.hpp"
 
-camera::camera(const rt::vector& origin, const rt::vector& direction) {
+camera::camera(const rt::vector& origin, const rt::vector& direction, const rt::vector& up, const rt::image& image, double distance) : _image(image) {
   _origin = origin;
   _direction = direction;
+  _distance = distance;
+  _up = up;
 }
 
+void camera::update_image() {
+  int width = _image.width();
+  int height = _image.height();
+  rt::vector center_of_image = _origin + _distance*_direction;
+  int x;
+  int y;
+  for (x=0; x<width; ++x) {
+    for (y=0; y<width; ++y) {
+
+    }
+  }
+}
--- src/camera.hpp
+++ src/camera.hpp
@@ -2,13 +2,18 @@
 #define __CAMERA_H
 
 #include "vector.hpp"
+#include "image.hpp"
 
 class camera {
   private:
     rt::vector _origin;
     rt::vector _direction;
+    rt::vector _up;
+    rt::image _image;
+    double _distance;
   public:
-    camera(const rt::vector& origin, const rt::vector& direction);
+    camera(const rt::vector& origin, const rt::vector& direction, const rt::vector& up, const rt::image& image, double distance);
+    void update_image();
 };
 
 #endif
