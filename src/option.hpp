231a4c0f8ed1b931129c21bf7114f91e149ad478
MONTILLET Xavier <xavierm02.net@gmail.com>
2013-11-24 10:22:38 +0100

    Modified option after according to answer of Luc.

--- src/option.hpp
+++ src/option.hpp
@@ -2,24 +2,26 @@
 #define __OPTION_H
 
 #include <stdexcept>
-
 template <typename T>
 class option {
 private:
-  bool _is_some;
-  const T& value;
-
-public:
-  option(bool is_some, const T& value) :
-    _is_some(is_some), value(value) {
+  bool is_defined;
+  union {
+    const T value;
+    bool _;
   };
 
-  bool is_some() const {
-    return _is_some;
+public:
+  option(bool is_defined, const T& value) :
+    is_defined(is_defined), value(value) {
+  };
+
+  bool get_is_defined() const {
+    return is_defined;
   };
 
   T get_value() const {
-    if (_is_some) {
+    if (is_defined) {
       return value;
     } else {
       throw (std::logic_error ("Trying to get value of an option equal to none."));
