4c965fb0a50634904bccb769e8f29a184464f27c
MONTILLET Xavier <xavierm02.net@gmail.com>
2013-11-26 09:17:49 +0100

    Updated demo.

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,13 @@ int main(void)
 
   Scene scene = Scene(rt::color(127, 127, 127));
 
-  Plane floor = Plane(Point(0,0,-1000), rt::vector(0,0,1), rt::color::WHITE);
-
   //Rectangle rect = Rectangle(Point(100,2000,-2000), rt::vector(1,0,1), rt::vector(0,1,0), 800, 800, rt::color::BLUE);
 
-  Sphere ball = Sphere(Point(100,0000,0), 400, rt::color::BLACK);
+  scene.add_object(Plane(Point(0,0,-1000), rt::vector(0,0,1), rt::color::WHITE));
+  scene.add_object(Sphere(Point(100,0,0), 400, rt::color::BLACK));
+  scene.add_object(Sphere(Point(600,-1200,-200), 400, rt::color::BLACK));
+  scene.add_object(Sphere(Point(600,1200,-200), 400, rt::color::BLACK));
 
-  scene.add_object(floor);
-  //scene.add_object(rect);
-  scene.add_object(ball);
   scene.add_source(Source(Point(1000,0,2000), Light::RED));
   scene.add_source(Source(Point(0,1000,2000), Light::GREEN));
   scene.add_source(Source(Point(-1000,0,2000), Light::BLUE));
